3002ab0a77a36550709494a36c85496e8a6a929a
Bensu Erdoğan <bensuerdogan58@gmail.com>
2021-02-22 14:46:53 +0300

    made FluentValidation

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,11 +22,9 @@ namespace Business.Concrete
         }
 
         public IResult Add(Car car)
-        {
-            if (car.DailyPrice < 100)
-            {
-                return new ErrorResult(Messages.Error);
-            }
+        {   
+            ValidationTool.Validate(new CarValidator(), car);
+
             _carDal.Add(car);
             return new SuccessResult(Messages.Success);
         }
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.DailyPrice).GreaterThan(100).When(c => c.BrandId == 1);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.ModelYear).Must(StartWith2).WithMessage("model year 2 ile başlamalı");
+        }
+
+        private bool StartWith2(string arg)
+        {
+            return arg.StartsWith("2");
+        }
+    }
+}
